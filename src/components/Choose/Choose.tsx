d950eef332e7a2cdad18af69de13af69554cb13f
Akram <akr232009@gmail.com>
2024-02-20 11:06:38 +0600

    edit

--- src/components/Choose/Choose.tsx
+++ src/components/Choose/Choose.tsx
@@ -3,24 +3,27 @@ import choose from '../../../global/assets/images/choose.svg'
 
 const Choose = () => {
     return (
-        <div className={styles.choose}>
-            {/*<img src={choose} alt="choose"/>*/}
-            <div className={styles.headerChoose}>Why choose us?</div>
-            <div className={styles.paragraphChoose}>
-                In addition, there 5 more reasons why men <br/> prefer Manhattan Barbershop N.Y.C:
+        <div>
+            <div className={styles.choose}>
+                {/*<img src={choose} alt="choose"/>*/}
+                <div className={styles.headerChoose}>Why choose us?</div>
+                <div className={styles.paragraphChoose}>
+                    In addition, there 5 more reasons why men <br/> prefer Manhattan Barbershop N.Y.C:
+                </div>
+                <div className={styles.listChoose}>
+                    <ul>
+                        <li>Always welcoming environment</li>
+                        <li>Our masters focus on the quality</li>
+                        <li>We value both the time <br/> and the money of our clients</li>
+                        <li>We work only with high-quality, <br/> hypoallergenic premium products</li>
+                        <li>All surfaces and tools are cleaned, <br/> disinfected before and after using</li>
+                    </ul>
+                </div>
             </div>
-            <div className={styles.listChoose}>
-                <ul>
-                    <li>Always welcoming environment</li>
-                    <li>Our masters focus on the quality</li>
-                    <li>We value both the time and the money of our clients</li>
-                    <li>We work only with high-quality, hypoallergenic premium products</li>
-                    <li>All surfaces and tools are cleaned, disinfected before and after using</li>
-                </ul>
-            </div>
+            <div className={styles.}
         </div>
-)
-    ;
+    )
+        ;
 }
 
 export default Choose;
