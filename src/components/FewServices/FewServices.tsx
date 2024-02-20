bfb6748f7dd1ef16d578bfdfb16e682ade9d25d8
Akram <akr232009@gmail.com>
2024-02-20 11:32:30 +0600

    edit

--- src/components/FewServices/FewServices.tsx
+++ src/components/FewServices/FewServices.tsx
@@ -1,10 +1,10 @@
 import styles from './FewServices.module.scss'
 import OneService from "../OneService/OneService"
 import {service_data} from "../OneService/servicedata.tsx"
-import imageScissors from '../../../global/assets/images/scissors.svg'
-import imageBeard from '../../../global/assets/images/beard.svg'
-import imageShaver from '../../../global/assets/images/shaver.svg'
-import imageWoman from '../../../global/assets/images/woman.svg'
+import scissors from '../../../global/assets/images/scissors.svg'
+import beard from '../../../global/assets/images/beard.svg'
+import shaver from '../../../global/assets/images/shaver.svg'
+import woman from '../../../global/assets/images/woman.svg'
 
 const FewServices = () => {
     const service_1 = service_data[0];
@@ -13,10 +13,10 @@ const FewServices = () => {
     const service_4 = service_data[3];
     return (
         <div className={styles.fewServices}>
-            <OneService image={imageScissors} list={[service_1]} />
-            <OneService image={imageBeard} list={[service_2]} />
-            <OneService image={imageShaver} list={[service_3]} />
-            <OneService image={imageWoman} list={[service_4]} />
+            <OneService image={scissors} list={[service_1]} />
+            <OneService image={beard} list={[service_2]} />
+            <OneService image={shaver} list={[service_3]} />
+            <OneService image={woman} list={[service_4]} />
         </div>
     );
 }
