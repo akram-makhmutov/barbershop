968bfb3b0e6c53e59f0655eb477206e3157dd1a6
Akram Makhmutov <57263419+akram-makhmutov@users.noreply.github.com>
2024-02-29 19:01:35 +0600

    Create Field.tsx

---
+++ src/components/Field/Field.tsx
@@ -0,0 +1,13 @@
+import styles from './Field.module.scss'
+
+const Field = ({height, text}: { height?: number, text: string }) => {
+    return (
+        <div className={styles.contactField}>
+            <div className={styles.whiteSquare} style={{height: height || 66}}>
+                <div className={styles.fieldText}>{text}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Field;
