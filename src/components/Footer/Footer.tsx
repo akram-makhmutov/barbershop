91866ac0e86629b7d5cf5ffba42c47a15917cec7
Akram Makhmutov <57263419+akram-makhmutov@users.noreply.github.com>
2024-02-29 18:59:38 +0600

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,2 +1,20 @@
 import styles from './Footer.module.scss'
-import stylish from
+import footer from '../../../global/assets/images/footer.svg'
+import Field from '../Field/Field.tsx'
+
+const Footer = () => {
+    return (
+        <div className={styles.FooterContainer} style={{backgroundColor: '#292D33'}}>
+            <div className={styles.contactContainer}>
+                <img src={footer} alt="footer"/>
+                <div className={styles.contactText}>Contact Us!</div>
+                <Field height={66} text="Enter your name"/>
+                <Field height={66} text="Enter a valid email address"/>
+                <Field height={210} text="Enter your message"/>
+            </div>
+        </div>
+    )
+        ;
+}
+
+export default Footer;
