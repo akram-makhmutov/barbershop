7f74abc523c78764f259cfbdf23b06137b152bae
Akram <akr232009@gmail.com>
2024-03-05 15:41:49 +0600

    edit

--- src/components/More/More.tsx
+++ src/components/More/More.tsx
@@ -1,8 +1,16 @@
 import styles from './More.module.scss'
+import {useState} from "react";
 
 const More = () => {
+    const [showMoreText, setShowMoreText] = useState(false);
+    const toggleMoreText = () => {
+        setShowMoreText(!showMoreText);
+    };
     return (
-        <div className={styles.buttonMore}>MORE</div>
+        <div>
+            {showMoreText && <div className={styles.hiddenText}>Тут ваш скрытый текст</div>}
+            <div onClick={toggleMoreText} className={styles.buttonMore}>MORE</div>
+        </div>
     );
 }
 
--- src/components/OneType/OneType.tsx
+++ src/components/OneType/OneType.tsx
@@ -1,4 +1,3 @@
-import React, {useState} from 'react';
 import styles from "./OneType.module.scss"
 import More from "../More/More.tsx"
 
@@ -8,23 +7,18 @@ interface Props {
 }
 
 const OneType = ({image, list}: Props) => {
-    const [showMoreText, setShowMoreText] = useState(false);
-    const toggleMoreText = () => {
-        setShowMoreText(!showMoreText);
 
-        return (
-            <div>
-                <img src={image} alt="image"/>
-                {list && list.map((item, index) => (
-                    <div key={index} className={styles.oneType}>
-                        <div className={styles.oneTypeText}>{item.name}</div>
-                    </div>
-                ))}
-                {showMoreText && <div className={styles.moreText}>Тут ваш скрытый текст</div>}
-                <button onClick={toggleMoreText} className={styles.moreButton}>More</button>
-                <More/>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <img src={image} alt="image"/>
+            {list && list.map((item, index) => (
+                <div key={index} className={styles.oneType}>
+                    <div className={styles.oneTypeText}>{item.name}</div>
+                </div>
+            ))}
+            <More/>
+        </div>
+    );
+}
 
-    export default OneType;
+export default OneType;
