fc996c36a95fb40bad0df03440c145a709e835f7
Akram <akr232009@gmail.com>
2024-03-06 16:38:33 +0600

    edit

--- src/components/More/More.tsx
+++ src/components/More/More.tsx
@@ -2,18 +2,17 @@ import styles from './More.module.scss';
 import {useState} from 'react';
 
 interface Props {
-    list: { name: string, text: string }[];
+    text: string;
 }
 
-const More = ({list}: Props) => {
+const More = ({text}: Props) => {
     const [showText, setShowText] = useState(false);
     const toggleMoreText = () => {
         setShowText(!showText);
     };
     return (
         <div>
-            {list && list.map((item, index) => (
-                showText && <div key={index} className={styles.hiddenText}>{item.text}</div>))}
+            {showText && <div className={styles.hiddenText}>{text}</div>}
             <div onClick={toggleMoreText} className={styles.buttonMore}>MORE</div>
         </div>
     );
