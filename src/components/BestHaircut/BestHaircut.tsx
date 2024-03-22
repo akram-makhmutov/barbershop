e80b4a6424d1fd4dd1e819409c4d3abc430a1739
Akram <akr232009@gmail.com>
2024-03-22 12:47:25 +0600

    edit

--- src/components/BestHaircut/BestHaircut.tsx
+++ src/components/BestHaircut/BestHaircut.tsx
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom';
 import styles from './BestHaircut.module.scss';
 import best_1 from '../../../global/assets/images/best_1.svg';
 import best_2 from '../../../global/assets/images/best_2.svg';
@@ -7,21 +8,32 @@ import best_5 from '../../../global/assets/images/best_5.svg';
 import best_6 from '../../../global/assets/images/best_6.svg';
 
 const BestHaircut = () => {
+
+    const imageClick = () => {
+        const element = document.getElementById("faggot");
+        if (element) {
+            element.addEventListener('click', () => {
+                document.body.classList.add('blackBack');
+            });
+        }
+    }
+
     return (
         <div className={styles.bestHaircut} style={{backgroundColor: '#9D9570'}}>
             <div className={styles.bestHaircutText}>Experience the Best Haircut <br/> & Shave Services</div>
             <div className={styles.bestHaircutImages}>
-                <img src={best_1} alt="best_1"/>
-                <img src={best_2} alt="best_2"/>
-                <img src={best_3} alt="best_3"/>
+                <img id="faggot" src={best_1} alt="best_1" onClick={imageClick}/>
+                <img id="faggot" src={best_2} alt="best_2" onClick={imageClick}/>
+                <img id="faggot" src={best_3} alt="best_3" onClick={imageClick}/>
             </div>
             <div className={styles.bestHaircutImages}>
-                <img src={best_4} alt="best_4"/>
-                <img src={best_5} alt="best_5"/>
-                <img src={best_6} alt="best_6"/>
+                <img id="faggot" src={best_4} alt="best_4" onClick={imageClick}/>
+                <img id="faggot" src={best_5} alt="best_5" onClick={imageClick}/>
+                <img id="faggot" src={best_6} alt="best_6" onClick={imageClick}/>
             </div>
         </div>
     )
 }
 
 export default BestHaircut;
+ReactDOM.render(<BestHaircut/>, document.getElementById('root'));
