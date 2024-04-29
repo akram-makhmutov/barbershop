26d347881c12ec8980331f2e285607dcf903012a
Akram <akr232009@gmail.com>
2024-04-29 10:05:30 +0600

    modal image done

--- src/components/BookOnline/BookOnline.tsx
+++ src/components/BookOnline/BookOnline.tsx
@@ -1,10 +1,31 @@
+import {useState} from "react";
 import styles from './BookOnline.module.scss';
+
 
 const BookOnline = () => {
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openForm = () => {
+        setIsOpen(true);
+        // document.body.classList.add('modalImageOpen');
+    };
+
+    
+    const closeForm = () => {
+        setIsOpen(false);
+        // document.body.classList.remove('modalImageOpen');
+    };
+
     return (
-        <div className={styles.buttonBookOnline}>Book Online</div>
-    );
+        <div>
+            {isOpen && (
+                <div className={styles.buttonBookOnline} onClick={openForm}>Book Online</div>
+    )
+}
+</div>
+)
+    ;
 }
 
 export default BookOnline;
