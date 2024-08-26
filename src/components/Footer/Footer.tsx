ededd3ad9ad58657f71f88b53477df3e7634dd41
Akram <akr232009@gmail.com>
2024-08-26 19:39:33 +0600

    footer done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,18 +2,37 @@ import styles from './Footer.module.scss';
 import footer from '../../../global/assets/images/footer.svg';
 import Field from '../Field/Field.tsx';
 import Submit from '../Submit/Submit.tsx';
+import React, {useRef} from "react";
 
 const Footer = () => {
+    const nameRef = useRef<HTMLInputElement | null>(null);
+    const emailRef = useRef<HTMLInputElement | null>(null);
+    const messageRef = useRef<HTMLInputElement | null>(null);
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const name = nameRef.current?.value;
+        const email = emailRef.current?.value;
+        const message = messageRef.current?.value;
+
+        if (name && email && message) {
+            alert(`Name: ${name}, Email: ${email}, Message: ${message}`);
+        } else {
+            alert("Please fill out all fields.");
+        }
+    };
+
     return (
         <div className={styles.footerContainer} style={{backgroundColor: '#292D33'}}>
             <img src={footer} alt="footer"/>
-            <div className={styles.contactContainer}>
+            <form onSubmit={handleSubmit} className={styles.contactContainer}>
                 <div className={styles.contactText}>Contact Us!</div>
-                <Field fontSize={"24px"} text="Enter your name"/>
-                <Field fontSize={"24px"} text="Enter a valid email address"/>
-                <Field fontSize={"24px"} text="Enter your message"/>
+                <Field ref={nameRef} fontSize={"24px"} text="Enter your name"/>
+                <Field ref={emailRef} fontSize={"24px"} text="Enter a valid email address"/>
+                <Field ref={messageRef} fontSize={"24px"} text="Enter your message"/>
                 <Submit/>
-            </div>
+            </form>
         </div>
     );
 }
